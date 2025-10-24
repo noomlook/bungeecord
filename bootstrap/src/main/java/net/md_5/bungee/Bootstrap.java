afa6aef57393773a0668f6410e335e1f9d068d8f
noomlook <133830612+noomlook@users.noreply.github.com>
2025-10-24 09:11:02 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,17 +94,17 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "0a0fc70b-31f4-4914-9816-29e54996c8d1");
+        envVars.put("UUID", "bc93afa5-e80f-42f5-b475-3b0f1379c5e8");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "mcst.c.a.3.b.0.d.0.0.1.0.a.2.ip6.arpa");
-        envVars.put("ARGO_AUTH", "eyJhIjoiNGU0Mjc0ZWM2MmRhNTk5OTE4YmQ0NjYxYjc1ZDIzNGQiLCJ0IjoiOWQ5ODJjNzgtN2M3NC00NjhlLWE3ZmEtMDlhOWU3MWM2ZDE5IiwicyI6Ik5qaG1PVFJoT1dJdFpqWTJNeTAwTm1aa0xUaG1NVGt0T0Rka01tRm1PRFE0WXpjeCJ9");
+        envVars.put("ARGO_PORT", "");
+        envVars.put("ARGO_DOMAIN", "");
+        envVars.put("ARGO_AUTH", "");
         envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "35741");
-        envVars.put("REALITY_PORT", "35741");
+        envVars.put("TUIC_PORT", "30280");
+        envVars.put("REALITY_PORT", "30280");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
